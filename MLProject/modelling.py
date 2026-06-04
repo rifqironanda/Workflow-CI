d2a76f3af8abc9259f5c5d6f56c984cdc8719af9
rifqironanda <rifqironanda@gmail.com>
2026-06-04 16:47:12 +0700

    Add training workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import mlflow
+import os
+import joblib
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import (
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score
-)
+from sklearn.metrics import accuracy_score
 
 # Load Data
 data = pd.read_csv("dataset_preprocess/telco_preprocessed.csv")
+
 X_train, X_test, y_train, y_test = train_test_split(
     data.drop("Churn", axis=1),
     data["Churn"],
@@ -20,22 +18,32 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=data["Churn"]
 )
 
-mlflow.set_experiment("basic_ml_model")
+mlflow.sklearn.autolog()
 
-# Training
-with mlflow.start_run():
-    mlflow.sklearn.autolog()
-    model = RandomForestClassifier(
-        n_estimators=200,
-        random_state=42
-    )
+model = RandomForestClassifier(
+    n_estimators=200,
+    random_state=42
+)
 
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+model.fit(X_train, y_train)
 
-    acc = accuracy_score(y_test, y_pred)
- 
-    print(f"Accuracy : {acc:.4f}")            
+y_pred = model.predict(X_test)
 
-    mlflow.log_metric("accuracy", acc)
+acc = accuracy_score(y_test, y_pred)
+
+print(f"Accuracy : {acc:.4f}")
+
+mlflow.log_metric("accuracy", acc)
+
+os.makedirs("saved_model", exist_ok=True)
+
+# Simpan model
+joblib.dump(model, "saved_model/model.pkl")
+
+# Simpan model MLflow
+mlflow.sklearn.save_model(
+    sk_model=model,
+    path="saved_model/mlflow_model"
+)
+
 print("Training selesai.")
